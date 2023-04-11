0afbba00bf1cfdb23a48286af867cb863600da33
Matthieu Chevreux <chevreux.matthieu@hotmail.com>
2023-04-11 23:37:36 +0200

    feat: keep track of the last cursed chat member

--- main.js
+++ main.js
@@ -10,24 +10,27 @@ const opts = {
     'squchan'
   ]
 };
+
 const client = new tmi.client(opts);
 
-var usersTracking = []; // [ { username: username, lastMessage: <timestamp in second> } ]
+var usersTracking = []; // [ { username: string, lastMessage: <timestamp in second>, isCursed: boolean } ]
 
 client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on('connected',  (addr, port) => { console.log(`* Connected to ${addr}:${port}`); });
 
 client.connect();
 
 function onMessageHandler (channel, tags, msg, self) {
+  const commandName = msg.trim();
+
   if (self) { return; }
 
-  if (usersTracking.filter( (elem) => elem.username === tags.username ).length == 0){
-    usersTracking.push({ username: tags.username, lastMessage: tags['tmi-sent-ts'] });
+  if (usersTracking.filter( (elem) => elem.username === tags.username).length == 0){
+    usersTracking.push({ username: tags.username, lastMessage: tags['tmi-sent-ts'], isCursed: false });
   } else {
     usersTracking = usersTracking.map((elem) => {
       if (elem.username === tags.username){
-        return { username: elem.username, lastMessage: tags['tmi-sent-ts'] }
+        return { username: elem.username, lastMessage: tags['tmi-sent-ts'], isCursed: elem.isCursed };
       }
       return elem;
     });
@@ -38,16 +41,26 @@ function onMessageHandler (channel, tags, msg, self) {
   usersTracking = usersTracking.filter( (elem) => {
     return (now - elem.lastMessage) <= 600000
   });
-}
 
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+  if (commandName === "!cursed"){
+    if (usersTracking.filter((elem) => elem.isCursed).length === 0){
+      client.say(channel, `Nobody is cursed at the moment, time to gift SquDink`);
+      return;
+    }
+    client.say(channel, `${usersTracking.filter((elem) => elem.isCursed)[0]}'s dick is a noodle ! everybody look at him and laugh KEKWPoint !`);
+  }
 }
 
 client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
   if (numbOfSubs === 5 && usersTracking.length > 0){
     const random = Math.floor(Math.random() * usersTracking.length);
+    usersTracking = usersTracking.map((elem) => {
+      if (elem.username === usersTracking[random].username){
+        return { username: elem.username, lastMessage: elem.lastMessage, isCursed: true }
+      }
+      return { username: elem.username, lastMessage: elem.lastMessage, isCursed: false }
+    });
+
     console.log(`${channel} Skedaddle Skedoodle ${usersTracking[random].username}'s dick is now a noodle !`);
     client.say(channel, `Skedaddle Skedoodle ${usersTracking[random].username}'s dick is now a noodle !`);
   }
